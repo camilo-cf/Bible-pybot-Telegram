0da5c6c169910c71934e9394db870fcfd62e5844
Camilo Cáceres <camilokceres@gmail.com>
2022-01-30 17:37:01 -0500

    Improved: Docker paths and pipelines

--- build_run.sh
+++ build_run.sh
@@ -1,2 +1,2 @@
-docker build --build-arg BOT_TOKEN=${BOT_TOKEN} -t biblepybottelegram .
+docker build --build-arg BOT_TOKEN=${BOT_TOKEN} -f docker/ubuntu/Dockerfile -t biblepybottelegram .
 docker run --rm -v ${PWD}:/app  -d biblepybottelegram

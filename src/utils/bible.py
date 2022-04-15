5b09b8e712d4cb65dc8752d237fcfb0caf66c312
Camilo Cáceres <camilokceres@gmail.com>
2022-04-14 20:15:17 -0500

    Improved libraried and added unit tests

--- src/utils/bible.py
+++ src/utils/bible.py
@@ -3,123 +3,59 @@
 """
 from difflib import SequenceMatcher
 import json
-from googletrans import Translator
 import numpy as np
 import requests
 
+import utils.validations as validations
 import utils.constants as constants
 
-########################################################################
-######################         Constants          ######################
-########################################################################
+# Constants
 Books = constants.BOOKS
 Dict_books = constants.DICT_BOOKS
 JSON_API_URL = constants.JSON_API_URL
 JSON_API_URL_2PART = constants.JSON_API_URL_2PART
-########################################################################
 
 
-########################################################################
-######################         Functions          ######################
-########################################################################
-# Constructor of the translator
-translator = Translator()
-
-
-def similar(string_1, string_2):
-    """
-    Compares to strings and verify their similarity ratio
+def get_chapter(book: str, chapter: int, bible_version: str = "akjv"):
+    """Get the Bible chapter
 
     Args:
-        string_1 (str): String to be compared
-        string_2 (str): String to be compared
+        book (str):
+            Given book of the Bible
 
-    Return:
-        similarity ratio (float betweenn 0.0 - 1.0)
-    """
-    return SequenceMatcher(None, string_1, string_2).ratio()
+        chapter (int):
+            Given chapter number
 
-
-def verify_book(book, books=Books):
-    """
-    Verify and Identify the book (input) according to the Books list,
-    returns the most similar book name.
-
-    Args:
-        book (str): Input of the user you want to verify
-        books (list): List of known books (given by default in constants)
-
-    Return:
-        The most similar book to the input in the list (str)
-    """
-    similarity_vec = [similar(book, each) for each in books]
-    return books[np.argmax(similarity_vec)]
-
-
-def verify_book_chapter(book, chapter, bible_version="akjv"):
-    """
-    Verify the if the given chapter of a given book exists.
-
-    Args:
-        book (str): Given book of the Bible
-        chapter (int): Given chapter number
-        bible_version (str): Version of the Bible
-
-    Return:
-        boolean telling if the chapter exists (True) or no (False)
-    """
-
-    try:
-        requesting = requests.get(
-            JSON_API_URL
-            + str(book)
-            + str(chapter)
-            + JSON_API_URL_2PART
-            + bible_version
-        )
-        text = requesting.text[1:-2]
-        json.loads(text)  # Only to check if is something there
-        return True
-    except (Exception) as exception:
-        print(exception)
-        return False
-
-
-def get_chapter(book, chapter, bible_version="akjv"):
-    """
-    Gey the Bible chapter
-
-    Args:
-        book (str): Given book of the Bible
-        chapter (int): Given chapter number
-        bible_version (str): Version of the Bible
+        bible_version (str):
+            Version of the Bible
 
     Return:
         The message of the bible in the given book and chapter (str) [it can be long]
     """
     requesting = requests.get(
-        JSON_API_URL
-        + str(book)
-        + str(chapter)
-        + JSON_API_URL_2PART
-        + bible_version
+        (
+            ((JSON_API_URL + book) + str(chapter) + JSON_API_URL_2PART)
+            + bible_version
+        )
     )
     return requesting.text[1:-2]
 
 
-def get_message(message, bible_version="akjv"):
-    """
-    Get the passage of the Bible given an income message.
+def get_message(message: str, bible_version: str = "akjv"):
+    """Get the passage of the Bible given an income message.
 
     Args:
-        message (str):  An input text that should be something like
-                        "John 14:6" or "Exodus 17:1-6" or "Genesis 1".
-        bible_version (str): Version of the Bible.
+        message (str):
+            An input text that should be something like
+            "John 14:6" or "Exodus 17:1-6" or "Genesis 1".
+
+        bible_version (str):
+            Version of the Bible.
 
     Return:
         The message of the bible in the given book and chapter (str) [it can be long].
     """
-    book = verify_book(message)
+    book = validations.verify_book(message)
     details = message.split(" ")[-1]
     message = " ".join([book, details])
 
@@ -156,28 +92,15 @@ def get_message(message, bible_version="akjv"):
     return "".join(full_verses)
 
 
-def translate_message(text, language="English", src="auto"):
-    """
-    Translates a text from an automatic source language to English
+def get_next_chapter(present_chapter: str, bible_version: str = "akjv"):
+    """Returns the next chapter in a given book and chapter
 
     Args:
-        text (str): text to be translated
-        language (str): objective language
-        src (str): source language
+        present_chapter (str):
+            String with a book of the bible and chapter, for example "John 1"
 
-    Return:
-        Translated text into the goal language
-    """
-    return translator.translate(text, dest=language, src=src).text
-
-
-def get_next_chapter(present_chapter, bible_version="akjv"):
-    """
-    Returns the next chapter in a given book and chapter
-
-    Args:
-        present_chapter (str): String with a book of the bible and chapter, for example "John 1"
-        bible_version (str): Version of the Bible
+        bible_version (str):
+            Version of the Bible
 
     Returns:
         text describing the next book and chapter
@@ -187,7 +110,7 @@ def get_next_chapter(present_chapter, bible_version="akjv"):
         > John 2
 
     """
-    book = verify_book(present_chapter)
+    book = validations.verify_book(present_chapter)
     chapter = present_chapter.split(" ")[-1]
     present_chapter = " ".join([book, chapter])
 
--- tests/test_bible.py
+++ tests/test_bible.py
@@ -7,60 +7,10 @@ import sys
 import string
 
 sys.path.append("./src/")
-import utils.constants as K
 from utils import bible
 
 
 class TestBible(unittest.TestCase):
-    def test_similar(self):
-        self.assertGreaterEqual(bible.similar("John", "Jonh"), 0.7)
-        self.assertGreaterEqual(bible.similar("John", "Jhn"), 0.7)
-        self.assertGreaterEqual(bible.similar("John", "Jon"), 0.7)
-        self.assertGreaterEqual(bible.similar("John", "Joh"), 0.7)
-
-    def test_verify_book(self):
-        for each in K.BOOKS:
-            word = each
-            word_list = list(word)
-            # Changes 2 random letters in the book name
-            for _ in range(1):
-                word_list[
-                    random.randint(0, len(word_list) - 1)
-                ] = random.choice(string.ascii_lowercase)
-            word = "".join(word_list)
-            if each not in [
-                "1Kings",
-                "2Kings",
-                "Job",
-                "Joel",
-                "1Peter",
-                "2Peter",
-                "3Peter",
-                "1Corinthians",
-                "2Corinthians",
-                "John",
-                "1John",
-                "2John",
-                "3John",
-                "Acts",
-                "Amos",
-                "1Samuel",
-                "2Samuel",
-                "1Chronicles",
-                "2Chronicles",
-                "1Timothy",
-                "2Timothy",
-                "1Thessalonians",
-                "2Thessalonians",
-                "Jude",
-                "Judges",
-            ]:
-                self.assertEqual(bible.verify_book(word, K.BOOKS), each)
-
-    def test_verify_book_chapter(self):
-        for book, n_chapters in K.TEST_BOOK_CHAPTERS.items():
-            self.assertEqual(bible.verify_book_chapter(book, n_chapters), True)
-
     def test_get_chapter(self):
         self.assertNotEqual(bible.get_chapter("Genesis", 1), None)
 
@@ -69,3 +19,6 @@ class TestBible(unittest.TestCase):
             bible.get_message("Genesis 1:1"),
             "1 In the beginning God created the heaven and the earth.\r\n",
         )
+
+    def test_next_chapter(self):
+        self.assertEqual(bible.get_next_chapter("John 1"), "John 2")

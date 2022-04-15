f63952c80beb08367523dc693f18998752a37703
Camilo Cáceres <camilokceres@gmail.com>
2022-04-14 20:15:17 -0500

    Improved libraried and added unit tests

---
+++ src/utils/validations.py
@@ -0,0 +1,169 @@
+"""Languages support module
+"""
+from difflib import SequenceMatcher
+from googletrans import Translator
+import logging as log
+import json
+import numpy as np
+import requests
+
+
+from utils import bible as Bible
+from utils import constants
+
+translator = Translator()
+
+# Available Languages and Versions in the API
+dict_api_version = constants.dict_api_version
+dict_api_ver2acr = constants.dict_api_ver2acr
+dict_api_acr2ver = constants.dict_api_acr2ver
+unique_languages_api = constants.unique_languages_api
+
+# Languages comparison
+languages_translation_set = constants.languages_translation_set
+lang_ver_transl = constants.lang_ver_transl
+lang_transl = constants.lang_transl
+
+# Other constants
+Books = constants.BOOKS
+Dict_books = constants.DICT_BOOKS
+JSON_API_URL = constants.JSON_API_URL
+JSON_API_URL_2PART = constants.JSON_API_URL_2PART
+
+
+def similar(string_1: str, string_2: str) -> float:
+    """
+    Compares to strings and verify their similarity ratio
+
+    Args:
+        string_1 (str):
+            String to be compared
+
+        string_2 (str):
+            String to be compared
+
+    Return:
+        Similarity ratio (float beteween 0.0 - 1.0)
+    """
+    return SequenceMatcher(None, string_1, string_2).ratio()
+
+
+def verify_book(book: str, books: list = Books):
+    """
+    Verify and Identify the book (input) according to the Books list,
+    returns the most similar book name.
+
+    Args:
+        book (str):
+            Input of the user you want to verify
+
+        books (list):
+            List of known books (given by default in constants)
+
+    Return:
+        The most similar book to the input in the list (str)
+    """
+    similarity_vec = [similar(book, each) for each in books]
+    return books[np.argmax(similarity_vec)]
+
+
+def verify_book_chapter(book: str, chapter: str, bible_version: str = "akjv"):
+    """
+    Verify the if the given chapter of a given book exists.
+
+    Args:
+        book (str): Given book of the Bible
+        chapter (int): Given chapter number
+        bible_version (str): Version of the Bible
+
+    Return:
+        boolean telling if the chapter exists (True) or no (False)
+    """
+
+    try:
+        requesting = requests.get(
+            (
+                (((JSON_API_URL + book) + chapter) + JSON_API_URL_2PART)
+                + bible_version
+            )
+        )
+
+        text = requesting.text[1:-2]
+        json.loads(text)  # Only to check if is something there
+        return True
+    except Exception as exception:
+        print(exception)
+        return False
+
+
+def verify_language(
+    language_input: str,
+    lang_ver_transl: list = list(lang_ver_transl),
+    lang_transl: list = list(lang_transl),
+) -> str:
+    """Function to verify the desired language
+
+    Args:
+        language_input (str):
+            Desired language.
+
+        lang_ver_transl (list, optional):
+            Defaults to list(lang_ver_transl).
+
+        lang_transl (list, optional):
+            Defaults to list(lang_transl).
+
+    Returns:
+        A string with the suggested language
+    """
+    languages = lang_transl + lang_ver_transl
+    language_input = translator.translate(language_input, dest="english").text
+    similarity_vec = [similar(language_input, each) for each in languages]
+    return languages[np.argmax(similarity_vec)]
+
+
+def verify_version(
+    version_input: str, versions: list = list(dict_api_ver2acr.keys())
+) -> str:
+    """Function to verify the bible version.
+
+    Args:
+        version_input (str):
+            Desired Bible version input.
+
+        versions (list, optional):
+            Default list of the available Bible versions.
+
+    Returns:
+        A string with the suggested bible version
+    """
+    similarity_vec = [similar(version_input, each) for each in versions]
+    return versions[np.argmax(similarity_vec)]
+
+
+def translate_message(
+    message: str, language: str = "English", src: str = "auto"
+):
+    """
+    Translates a text from an automatic source language to English
+
+    Args:
+        message (str):
+            Message to translate.
+
+        language (str):
+            Objective language
+
+        src (str):
+            Source language
+
+    Return:
+        Translated text into the goal language
+    """
+    try:
+        return translator.translate(
+            message, dest=language, src=src
+        ).text.replace("/ ", "/")
+    except (Exception) as exception:
+        log.critical("In send translated message - Exception: ", exception)
+        print("Connection Error")
---
+++ tests/test_validations.py
@@ -0,0 +1,78 @@
+"""
+    Unit tests for the Bible class.
+"""
+import unittest
+import random
+import sys
+import string
+
+sys.path.append("./src/")
+import utils.constants as K
+import utils.validations as validations
+
+# for book, n_chapters in K.TEST_BOOK_CHAPTERS.items():
+#     print(validations.verify_book_chapter(book, str(n_chapters)))
+
+class TestValidations(unittest.TestCase):
+    def test_similar(self):
+        self.assertGreaterEqual(validations.similar("John", "Jonh"), 0.7)
+        self.assertGreaterEqual(validations.similar("John", "Jhn"), 0.7)
+        self.assertGreaterEqual(validations.similar("John", "Jon"), 0.7)
+        self.assertGreaterEqual(validations.similar("John", "Joh"), 0.7)
+
+    def test_verify_book(self):
+        for each in K.BOOKS:
+            word = each
+            word_list = list(word)
+            # Changes 2 random letters in the book name
+            for _ in range(1):
+                word_list[
+                    random.randint(0, len(word_list) - 1)
+                ] = random.choice(string.ascii_lowercase)
+            word = "".join(word_list)
+            if each not in [
+                "1Kings",
+                "2Kings",
+                "Job",
+                "Joel",
+                "1Peter",
+                "2Peter",
+                "3Peter",
+                "1Corinthians",
+                "2Corinthians",
+                "John",
+                "1John",
+                "2John",
+                "3John",
+                "Acts",
+                "Amos",
+                "1Samuel",
+                "2Samuel",
+                "1Chronicles",
+                "2Chronicles",
+                "1Timothy",
+                "2Timothy",
+                "1Thessalonians",
+                "2Thessalonians",
+                "Jude",
+                "Judges",
+            ]:
+                self.assertEqual(validations.verify_book(word, K.BOOKS), each)
+
+    def test_verify_book_chapter(self):
+        for book, n_chapters in K.TEST_BOOK_CHAPTERS.items():
+            self.assertTrue(
+                validations.verify_book_chapter(book, str(n_chapters))
+            )
+
+    def test_verify_language(self):
+        self.assertEqual(validations.verify_language("sparnish"), "Spanish")
+
+    def test_verify_version(self):
+        self.assertEqual(
+            validations.verify_version("KJV").replace(" ", ""),
+            "KingJamesVersion",
+        )
+
+    def test_translate_message(self):
+        self.assertEqual(validations.translate_message("hola"), "hello")

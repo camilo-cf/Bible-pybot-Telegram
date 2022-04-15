d328447ae500d700ad2d5d70bdaa0a96f4545a44
Camilo Cáceres <camilokceres@gmail.com>
2022-04-15 16:03:05 -0500

    Improved bible tests

--- tests/test_bible.py
+++ tests/test_bible.py
@@ -15,10 +15,30 @@ class TestBible(unittest.TestCase):
         self.assertNotEqual(bible.get_chapter("Genesis", 1), None)
 
     def test_get_message(self):
+        GEN_1_1 = "1 In the beginning God created the heaven and the earth.\r\n"
+        self.assertEqual(
+            bible.get_message("Genesis 1:1."),
+            GEN_1_1,
+        )
         self.assertEqual(
             bible.get_message("Genesis 1:1"),
-            "1 In the beginning God created the heaven and the earth.\r\n",
+            GEN_1_1,
+        )
+        self.assertTrue(
+            GEN_1_1 in bible.get_message("Genesis 1")
+            )
+        self.assertEqual(
+            bible.get_message("Genesis 100:100"),
+            "Error",
+        )
+        self.assertEqual(
+            bible.get_message("Genesis"),
+            "Error - Verify the chapter of the passage",
         )
 
     def test_next_chapter(self):
         self.assertEqual(bible.get_next_chapter("John 1"), "John 2")
+        self.assertEqual(bible.get_next_chapter("John 1."), "John 2")
+        self.assertEqual(bible.get_next_chapter("John 21"), "Acts 1")
+        self.assertEqual(bible.get_next_chapter("John 21."), "Acts 1")
+        self.assertEqual(bible.get_next_chapter("John"), "Genesis 1")

d20aec03d1f90da12e0cec80e1a75de8217d44d5
Camilo Cáceres <camilokceres@gmail.com>
2022-04-15 16:55:05 -0500

    Minor improvement on sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append(os.path.abspath('../../src/'))
 sys.path.append(os.path.abspath('../../src/utils/'))
+sys.path.append(os.path.abspath('../../src/telegram_pages/'))
 
 
 # -- Project information -----------------------------------------------------
